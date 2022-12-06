b520180c51bf1f340d4a032e35f65c3208759455
Nia Robinson <nrobinson00046@gmail.com>
2022-12-06 11:36:33 -0500

    Updated Web3 settings.

--- crypto_wallet.py
+++ crypto_wallet.py
@@ -11,10 +11,11 @@ from dotenv import load_dotenv
 load_dotenv()
 from bip44 import Wallet
 from web3 import Account
-from web3.auto.infura.kovan import w3
 from web3 import middleware
 from web3.gas_strategies.time_based import medium_gas_price_strategy
+from web3 import Web3
 
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
 ################################################################################
 # Wallet functionality
 
